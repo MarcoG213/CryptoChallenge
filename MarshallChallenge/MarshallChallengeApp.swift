aa6e5a3053451b2e2220d4cc594337926c00e68e
marcoG <marcog.swe@icloud.com>
2025-04-25 12:13:37 +0200

    Initial Commit

---
+++ MarshallChallenge/MarshallChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarshallChallengeApp.swift
+//  MarshallChallenge
+//
+//  Created by MarcoG on 2025-04-25.
+//
+
+import SwiftUI
+
+@main
+struct MarshallChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
