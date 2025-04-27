aec1a5cbdd9e140ebb2715e400967d835b645e38
marcoG <marcog.swe@icloud.com>
2025-04-27 17:41:51 +0200

    API module - updated model

--- API/Sources/API/Models/RawCryptoResponse.swift
+++ API/Sources/API/Models/RawCryptoResponse.swift
@@ -36,6 +36,8 @@ extension RawCryptoResponse {
         let changeFactor = Double.random(in: -0.05...0.05)
         let last = open * (1.0 + changeFactor)
         
+        let marketCap = Double.randomMarketCap(for: open)
+        
         let change24h = ((last - open) / open) * 100
         let timestamp = Date(timeIntervalSince1970: at / 1000)
 
@@ -45,11 +47,32 @@ extension RawCryptoResponse {
             quoteAsset: quoteAsset,
             price: last,
             openPrice: open,
+            currentPrice: last,
             highPrice: high,
             lowPrice: low,
             volume: vol,
             timestamp: timestamp,
-            change24h: change24h
+            change24h: change24h,
+            marketCap: marketCap
         )
+    }
+}
+
+fileprivate extension Double {
+    static func randomMarketCap(for price: Double) -> Double {
+        let multiplier: Double
+
+        switch price {
+        case 10000...:
+            multiplier = Double.random(in: 10_000_000...100_000_000)
+        case 1000..<10000:
+            multiplier = Double.random(in: 1_000_000...10_000_000)
+        case 10..<1000:
+            multiplier = Double.random(in: 100_000...1_000_000)
+        default:
+            multiplier = Double.random(in: 10_000...100_000)
+        }
+
+        return price * multiplier
     }
 }
