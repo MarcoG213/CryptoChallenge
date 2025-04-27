c715bd350109894d5d9134d6ef519af50f876507
marcoG <marcog.swe@icloud.com>
2025-04-27 18:44:20 +0200

    Design System module - Color extension

---
+++ DesignSystem/Sources/DesignSystem/Resources/Color+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  CryptoColor.swift
+//  DesignSystem
+//
+//  Created by MarcoG on 2025-04-27.
+//
+
+import SwiftUI
+
+public extension Color {
+    static var positive: Color {
+        Color("positive_green", bundle: .module)
+    }
+
+    static var negative: Color {
+        Color("negative_red", bundle: .module)
+    }
+}
