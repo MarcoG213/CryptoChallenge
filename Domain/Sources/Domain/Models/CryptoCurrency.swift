110e0bc57f38cc1d123a935c74429d108a2c1365
marcoG <marcog.swe@icloud.com>
2025-04-27 17:43:49 +0200

    Domain module - Updated model

--- Domain/Sources/Domain/Models/CryptoCurrency.swift
+++ Domain/Sources/Domain/Models/CryptoCurrency.swift
@@ -15,31 +15,37 @@ public struct CryptoCurrency: Identifiable, Codable, Currency, Sendable {
     public let quoteAsset: String
     public let price: Double
     public let openPrice: Double
+    public let currentPrice: Double
     public let highPrice: Double
     public let lowPrice: Double
     public let volume: Double
     public let timestamp: Date
     public let change24h: Double?
+    public let marketCap: Double?
 
     public init(symbol: String,
                 baseAsset: String,
                 quoteAsset: String,
                 price: Double,
                 openPrice: Double,
+                currentPrice: Double,
                 highPrice: Double,
                 lowPrice: Double,
                 volume: Double,
                 timestamp: Date,
-                change24h: Double?) {
+                change24h: Double?,
+                marketCap: Double?) {
         self.symbol = symbol
         self.baseAsset = baseAsset
         self.quoteAsset = quoteAsset
         self.price = price
         self.openPrice = openPrice
+        self.currentPrice = currentPrice
         self.highPrice = highPrice
         self.lowPrice = lowPrice
         self.volume = volume
         self.timestamp = timestamp
         self.change24h = change24h
+        self.marketCap = marketCap
     }
 }
