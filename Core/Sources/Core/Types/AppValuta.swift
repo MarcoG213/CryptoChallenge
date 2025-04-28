6ac28e595ec5e331ece7c0eb4f25fd7099756320
marcoG <marcog.swe@icloud.com>
2025-04-28 11:54:09 +0200

    Core module - types

---
+++ Core/Sources/Core/Types/AppValuta.swift
@@ -0,0 +1,13 @@
+//
+//  AppValuta.swift
+//  Domain
+//
+//  Created by MarcoG on 2025-04-28.
+//
+
+import Foundation
+
+public enum AppValuta: String, CaseIterable {
+    case usd = "USD"
+    case sek = "SEK"
+}
---
+++ Core/Sources/Core/Types/FilterOption.swift
@@ -0,0 +1,56 @@
+//
+//  FilterOption.swift
+//  Core
+//
+//  Created by MarcoG on 2025-04-28.
+//
+
+import Foundation
+
+public enum FilterOption : String, CaseIterable {
+    case valueAsc
+    case valueDesc
+    case change24hAsc
+    case change24hDesc
+    case marketCapAsc
+    case marketCapDesc
+    
+    public var title: String {
+        switch self {
+        case .valueAsc, .valueDesc:
+            return "Value"
+        case .change24hAsc, .change24hDesc:
+            return "24h Change"
+        case .marketCapAsc, .marketCapDesc:
+            return "Market Cap"
+        }
+    }
+    
+    public var isAscending: Bool {
+        switch self {
+        case .valueAsc, .change24hAsc, .marketCapAsc:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    public var baseOption: Self {
+        switch self {
+        case .valueAsc, .valueDesc: return .valueDesc
+        case .change24hAsc, .change24hDesc: return .change24hDesc
+        case .marketCapAsc, .marketCapDesc: return .marketCapDesc
+        }
+    }
+    
+    public func toggled() -> FilterOption {
+        switch self {
+        case .valueAsc: return .valueDesc
+        case .valueDesc: return .valueAsc
+        case .change24hAsc: return .change24hDesc
+        case .change24hDesc: return .change24hAsc
+        case .marketCapAsc: return .marketCapDesc
+        case .marketCapDesc: return .marketCapAsc
+        }
+    }
+}
